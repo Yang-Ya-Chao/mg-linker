12d95aed4043a527bdecad29126767125d17139d
YangYachao <909971934@qq.com>
2026-01-23 14:03:27 +0000

    Revert "Merge branch 'dev' of gitee.com:yangyachao-X/mg-linker into master"
    This reverts commit a2f47236e3358197f18508b86935491da575d832.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,9 +7,6 @@ pluginManagement {
                 includeGroupByRegex("androidx.*")
             }
         }
-        maven("https://maven.aliyun.com/repository/public") // 阿里云镜像
-        maven("https://maven.aliyun.com/repository/central") // Central 仓库镜像
-        maven("https://maven.aliyun.com/repository/gradle-plugin") // Gradle 插件镜像
         mavenCentral()
         gradlePluginPortal()
     }
@@ -17,8 +14,6 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://maven.aliyun.com/repository/public")
-        maven("https://maven.aliyun.com/repository/central")
         google()
         mavenCentral()
     }
