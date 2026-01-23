b1f2b7d1d22116f6f432ef29e2d459cfefd61bd0
yangyachao <909971934@qq.com>
2026-01-24 01:26:52 +0800

    1.增加图标版小组件 2.续航四位数显示修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "2.4"
+        versionName = "2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
